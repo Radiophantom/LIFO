82e48e85d6667cf0f43932893e31cdffee00af0c
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2022-07-26 16:40:04 +0300

    Some code refactoring changes

--- rtl/lifo.sv
+++ rtl/lifo.sv
@@ -2,50 +2,55 @@ module lifo #(
   parameter int DWIDTH = 8,
   parameter int AWIDTH = 4
 )(
-  input                         clk_i,
-  input                         srst_i,
+  input               clk_i,
+  input               srst_i,
 
-  input                         wrreq_i,
-  input        [DWIDTH - 1 : 0] data_i,
+  input               wrreq_i,
+  input  [DWIDTH-1:0] data_i,
 
-  input                         rdreq_i,
-  output logic [DWIDTH - 1 : 0] q_o,
+  input               rdreq_i,
+  output [DWIDTH-1:0] q_o,
 
-  output logic                  empty_o,
-  output logic                  full_o,
-  output logic [AWIDTH : 0]     usedw_o
+  output              empty_o,
+  output              full_o,
+  output [AWIDTH:0]   usedw_o
 );
 
 localparam LIFO_DEPTH = 2**AWIDTH;
 
-logic [DWIDTH - 1 : 0] mem [LIFO_DEPTH - 1 : 0];
+logic [DWIDTH-1:0] mem [0:LIFO_DEPTH-1];
 
-logic [AWIDTH - 1 : 0] wr_ptr;
-logic [AWIDTH - 1 : 0] rd_ptr;
+logic [AWIDTH-1:0] wr_ptr;
+logic [AWIDTH-1:0] rd_ptr;
+
+logic [DWIDTH-1:0] q;
+logic              empty;
+logic              full;
+logic [AWIDTH:0]   usedw;
 
 always_ff @( posedge clk_i )
   if( srst_i )
-    usedw_o <= '0;
+    usedw <= '0;
   else if( wrreq_i )
-    usedw_o <= usedw_o + 1'b1;
+    usedw <= usedw + 1'b1;
   else if( rdreq_i )
-    usedw_o <= usedw_o - 1'b1;
+    usedw <= usedw - 1'b1;
 
 always_ff @( posedge clk_i )
   if( srst_i )
-    full_o <= 1'b0;
+    full <= 1'b0;
   else if( rdreq_i )
-    full_o <= 1'b0;
-  else if( wrreq_i && ( usedw_o == ( LIFO_DEPTH - 1 ) ) )
-    full_o <= 1'b1;
+    full <= 1'b0;
+  else if( wrreq_i && ( usedw == ( LIFO_DEPTH - 1 ) ) )
+    full <= 1'b1;
 
 always_ff @( posedge clk_i )
   if( srst_i )
-    empty_o <= 1'b1;
+    empty <= 1'b1;
   else if( wrreq_i )
-    empty_o <= 1'b0;
-  else if( rdreq_i && ( usedw_o == 1 ) )
-    empty_o <= 1'b1;
+    empty <= 1'b0;
+  else if( rdreq_i && ( usedw == 1 ) )
+    empty <= 1'b1;
 
 // Sync RAM inferring
 always_ff @( posedge clk_i )
@@ -53,7 +58,7 @@ always_ff @( posedge clk_i )
     if( wrreq_i )
       mem[wr_ptr] <= data_i;
     if( rdreq_i )
-      q_o <= mem[rd_ptr];
+      q <= mem[rd_ptr];
   end
 
 always_ff @( posedge clk_i )
@@ -71,6 +76,11 @@ always_ff @( posedge clk_i )
     rd_ptr <= wr_ptr;
   else if( rdreq_i )
     rd_ptr <= rd_ptr - 1'b1;
+
+assign q_o      = q;
+assign empty_o  = empty;
+assign full_o   = full;
+assign usedw_o  = usedw;
 
 always @( posedge clk_i )
   begin
--- tb/make.tcl
+++ tb/make.tcl
@@ -1,6 +1,6 @@
 vlib work
-vlog -sv {../rtl/lifo.sv}
-vlog -sv {./lifo_tb.sv}
+vlog -sv ../rtl/lifo.sv
+vlog -sv ./lifo_tb.sv
 vsim lifo_tb
 do wave.do
 run -all
